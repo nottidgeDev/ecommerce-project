7d7f4d73b5986d171b5267b4cd0a57aec5181675
Nottidge Chibueze <nottidgechibueze042@gmail.com>
2025-06-20 12:39:56 +0100

    Create add to cart feature

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -79,7 +79,7 @@ export function OrdersPage({ cart }) {
                         </div>
                       </Fragment>
                     );
-                  })}z
+                  })}
                 </div>
               </div>
             );
